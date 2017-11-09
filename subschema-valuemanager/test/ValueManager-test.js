225a1fe5189aa5f31315617d1941a0f3174ab60c
Justin Spears <speajus@gmail.com>
2017-11-09 14:16:28 -0800

    move to chai

--- subschema-valuemanager/test/ValueManager-test.js
+++ subschema-valuemanager/test/ValueManager-test.js
@@ -1,5 +1,5 @@
 import ValueManager from '../lib/index';
-import expect from 'expect';
+import {expect} from 'chai';
 
 describe('ValueManager', function () {
     this.timeout(20000);
@@ -7,9 +7,9 @@ describe('ValueManager', function () {
         it('should add a listener and fire on change', function (done) {
             var vm = new ValueManager();
             vm.addListener('stuff', function (newValue, oldValue, path) {
-                expect(oldValue).toNotExist();
-                expect(newValue).toEqual(1);
-                expect(path).toEqual('stuff');
+                expect(oldValue).to.not.exist;
+                expect(newValue).to.eql(1);
+                expect(path).to.eql('stuff');
                 done();
             });
             vm.setValue({
@@ -22,9 +22,9 @@ describe('ValueManager', function () {
                 var vm = new ValueManager();
                 vm.addListener('stuff.more',
                     function (newValue, oldValue, path) {
-                        expect(oldValue).toNotExist();
-                        expect(newValue).toEqual(false);
-                        expect(path).toEqual('stuff.more');
+                        expect(oldValue).to.not.exist;
+                        expect(newValue).to.eql(false);
+                        expect(path).to.eql('stuff.more');
                         done();
 
                     });
@@ -42,14 +42,14 @@ describe('ValueManager', function () {
                 vm.addListener('stuff.more',
                     function (newValue, oldValue, path) {
                         if (first) {
-                            expect(oldValue).toNotExist();
-                            expect(newValue).toEqual(false);
-                            expect(path).toEqual('stuff.more');
+                            expect(oldValue).to.not.exist;
+                            expect(newValue).to.eql(false);
+                            expect(path).to.eql('stuff.more');
                             first = false;
                         } else {
-                            expect(oldValue).toEqual(false);
-                            expect(newValue).toEqual(true);
-                            expect(path).toEqual('stuff.more');
+                            expect(oldValue).to.eql(false);
+                            expect(newValue).to.eql(true);
+                            expect(path).to.eql('stuff.more');
 
 
                             done();
@@ -75,14 +75,14 @@ describe('ValueManager', function () {
                 vm.addListener('stuff.more',
                     function (newValue, oldValue, path) {
                         if (first) {
-                            expect(oldValue).toNotExist();
-                            expect(newValue).toEqual(false);
-                            expect(path).toEqual('stuff.more');
+                            expect(oldValue).to.not.exist;
+                            expect(newValue).to.eql(false);
+                            expect(path).to.eql('stuff.more');
                             first = false;
                         } else {
-                            expect(oldValue).toEqual(false);
-                            expect(newValue).toEqual(null);
-                            expect(path).toEqual('stuff.more');
+                            expect(oldValue).to.eql(false);
+                            expect(newValue).to.eql(null);
+                            expect(path).to.eql('stuff.more');
 
                             done();
                         }
@@ -104,14 +104,14 @@ describe('ValueManager', function () {
                 vm.addListener('stuff.more',
                     function (newValue, oldValue, path) {
                         if (first) {
-                            expect(oldValue).toNotExist();
-                            expect(newValue).toEqual(false);
-                            expect(path).toEqual('stuff.more');
+                            expect(oldValue).to.not.exist;
+                            expect(newValue).to.eql(false);
+                            expect(path).to.eql('stuff.more');
                             first = false;
                         } else {
-                            expect(oldValue).toEqual(false);
-                            expect(newValue).toEqual(null);
-                            expect(path).toEqual('stuff.more');
+                            expect(oldValue).to.eql(false);
+                            expect(newValue).to.eql(null);
+                            expect(path).to.eql('stuff.more');
 
                             done();
                         }
@@ -138,7 +138,7 @@ describe('ValueManager', function () {
             });
             vm.removeListener(added);
             vm.addListener('stuff.more', function () {
-                expect(fired).toEqual(1);
+                expect(fired).to.eql(1);
                 done();
             });
             vm.setValue(null);
@@ -158,7 +158,7 @@ describe('ValueManager', function () {
                 });
                 vm.removeListener('stuff.more');
                 vm.addListener('stuff.more', function () {
-                    expect(fired).toEqual(1);
+                    expect(fired).to.eql(1);
                     done();
                 });
                 vm.setValue(null);
@@ -174,16 +174,16 @@ describe('ValueManager', function () {
             );
             var fired = false;
             vm.addListener('stuff.more', function (newValue, oldValue, path) {
-                expect(oldValue).toEqual(true);
-                expect(newValue).toEqual(false);
-                expect(path).toEqual('stuff.more');
+                expect(oldValue).to.eql(true);
+                expect(newValue).to.eql(false);
+                expect(path).to.eql('stuff.more');
                 done();
             });
-            expect(vm.getValue().stuff.more).toEqual(true);
+            expect(vm.getValue().stuff.more).to.eql(true);
             vm.update('stuff.more', false);
             var val = vm.getValue();
-            expect(val.stuff.more).toEqual(false);
-            expect(val.other).toEqual(true);
+            expect(val.stuff.more).to.eql(false);
+            expect(val.other).to.eql(true);
         });
         it('should update change when it is nested and no value', function () {
             var vm    = new ValueManager(
@@ -194,16 +194,16 @@ describe('ValueManager', function () {
             );
             var fired = false;
             vm.addListener('stuff.more', function (newValue, oldValue, path) {
-                expect(oldValue).toNotExist();
-                expect(newValue).toEqual(false);
-                expect(path).toEqual('stuff.more');
+                expect(oldValue).to.not.exist;
+                expect(newValue).to.eql(false);
+                expect(path).to.eql('stuff.more');
                 fired = true;
             });
             vm.update('stuff.more', false);
             var val = vm.getValue();
-            expect(val.stuff.more).toEqual(false);
-            expect(val.other).toEqual(true);
-            expect(fired).toEqual(true);
+            expect(val.stuff.more).to.eql(false);
+            expect(val.other).to.eql(true);
+            expect(fired).to.eql(true);
         });
         it('should create an array', function (done) {
             var vm = new ValueManager(
@@ -213,15 +213,15 @@ describe('ValueManager', function () {
                 }
             );
             vm.addListener('stuff', function (newValue, oldValue, path) {
-                expect(oldValue).toNotExist();
-                expect(newValue[0].more).toEqual(false);
-                expect(path).toEqual('stuff.0.more');
+                expect(oldValue).to.not.exist;
+                expect(newValue[0].more).to.eql(false);
+                expect(path).to.eql('stuff.0.more');
                 done();
             });
             vm.update('stuff.0.more', false);
             var val = vm.getValue();
-            expect(val.stuff[0].more).toEqual(false);
-            expect(val.other).toEqual(true);
+            expect(val.stuff[0].more).to.eql(false);
+            expect(val.other).to.eql(true);
         });
         it('should replace all the keys', function () {
             var vm = new ValueManager(
@@ -233,9 +233,9 @@ describe('ValueManager', function () {
             );
             vm.setValue({ test: 1, other: false });
             var val = vm.getValue();
-            expect(val.other).toEqual(false);
-            expect(val.more).toNotExist();
-            expect(val.test).toEqual(1);
+            expect(val.other).to.eql(false);
+            expect(val.more).to.not.exist;
+            expect(val.test).to.eql(1);
         });
     });
     describe('error handling', function () {
@@ -244,8 +244,8 @@ describe('ValueManager', function () {
             var vm     = ValueManager({ other: true },
                 { other: [{ message: 'Has Error' }] });
             var errors = vm.errorsFor('other');
-            expect(errors.length).toEqual(1);
-            expect(errors[0].message).toEqual('Has Error');
+            expect(errors.length).to.eql(1);
+            expect(errors[0].message).to.eql('Has Error');
 
         });
         it('should have errors nested errors', function () {
@@ -254,9 +254,9 @@ describe('ValueManager', function () {
                 'other.more': [{ message: 'More' }]
             });
             var errors = vm.errorsFor('other');
-            expect(errors.length).toEqual(2);
-            expect(errors[0].message).toEqual('Has Error');
-            expect(errors[1].message).toEqual('More');
+            expect(errors.length).to.eql(2);
+            expect(errors[0].message).to.eql('Has Error');
+            expect(errors[1].message).to.eql('More');
 
         });
         it('should fire error listeners', function () {
@@ -266,17 +266,17 @@ describe('ValueManager', function () {
                 errors.push(Array.prototype.slice.call(arguments));
             });
             vm.updateErrors('other', [{ message: 'Has Error' }]);
-            expect(errors.length).toEqual(1);
+            expect(errors.length).to.eql(1);
 
             vm.updateErrors('other.more', { message: 'Has More' });
-            expect(errors.length).toEqual(2);
+            expect(errors.length).to.eql(2);
 
             var e = vm.getErrors();
-            expect(e['other.more'].length).toEqual(1);
-            expect(e['other'].length).toEqual(1);
+            expect(e['other.more'].length).to.eql(1);
+            expect(e['other'].length).to.eql(1);
             vm.updateErrors('other', null);
             var e = vm.getErrors();
-            expect(e['other']).toNotExist();
+            expect(e['other']).to.not.exist;
 
         });
     });
@@ -292,10 +292,10 @@ describe('ValueManager', function () {
 
             vm.update('hello.world', 'Don');
 
-            expect(vm.path('hello.world')).toBe('Don');
+            expect(vm.path('hello.world')).to.eql('Don');
 
             vm.unstash('test1', idx);
-            expect(vm.path('hello.world')).toBe('Joe');
+            expect(vm.path('hello.world')).to.eql('Joe');
 
         })
 
@@ -312,12 +312,12 @@ describe('ValueManager', function () {
 
             vm.stash('test1', ['hello']);
 
-            expect(vm.path('hello.world')).toBe('Don');
+            expect(vm.path('hello.world')).to.eql('Don');
 
             vm.unstash('test1');
-            expect(vm.path('hello.world')).toBe('Don');
+            expect(vm.path('hello.world')).to.eql('Don');
             vm.unstash('test1');
-            expect(vm.path('hello.world')).toBe('Joe');
+            expect(vm.path('hello.world')).to.eql('Joe');
 
 
         });
@@ -335,12 +335,12 @@ describe('ValueManager', function () {
 
             vm.stash('test1');
 
-            expect(vm.path('hello.world')).toBe('Don');
+            expect(vm.path('hello.world')).to.eql('Don');
 
             vm.unstash('test1');
-            expect(vm.path('hello.world')).toBe('Don');
+            expect(vm.path('hello.world')).to.eql('Don');
             vm.unstash('test1');
-            expect(vm.path('hello.world')).toBe('Joe');
+            expect(vm.path('hello.world')).to.eql('Joe');
 
 
         })
@@ -358,12 +358,12 @@ describe('ValueManager', function () {
 
             vm.stash();
 
-            expect(vm.path('hello.world')).toBe('Don');
+            expect(vm.path('hello.world')).to.eql('Don');
 
             vm.unstash();
-            expect(vm.path('hello.world')).toBe('Don');
+            expect(vm.path('hello.world')).to.eql('Don');
             vm.unstash();
-            expect(vm.path('hello.world')).toBe('Joe');
+            expect(vm.path('hello.world')).to.eql('Joe');
 
 
         })
