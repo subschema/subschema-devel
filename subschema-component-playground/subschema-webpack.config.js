aa32e7ef31a30b544d001594fef810c65731bb1e
Justin <speajus@gmail.com>
2017-11-11 16:36:55 -0800

    Fix versions (#16)
    * fixed demo option -- whoops
    
    * v2.2.11
    
    * making entry work better
    
    * fix arg
    
    * kinda works
    
    * tests pass
    
    * tests pass clean build
    
    * update react
    
    * changed to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * move to chai
    
    * test pass
    
    * got enzyme working
    
    * removed dead deps
    
    * change node version
    
    * upgrade yarn
    
    * upgrade yarn
    
    * upgrade yarn
    
    * upgrade yarn
    
    * upgrade yarn
    
    * v3.0.0-b1
    
    * test pass
    
    * update loader
    
    * added test
    
    * added test
    
    * remove lodash deps and fix things
    
    * v3.0.0-b2
    
    * making wiki better
    
    * fixes entry parseing

--- subschema-component-playground/subschema-webpack.config.js
+++ subschema-component-playground/subschema-webpack.config.js
@@ -1,9 +1,8 @@
 module.exports = function (options, webpack) {
 
 
-    webpack.resolve.alias['subschema-raw'] =
+    webpack.resolve.alias['subschema-raw']       =
         require.resolve('subschema/dist/subschema-debug.js');
-
     (webpack.node || (webpack.node = {})).buffer = false;
 
     return webpack;
