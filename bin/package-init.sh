a6690f962914b2bb78eb850dfdacdcec22889027
Justin Spears <speajus@gmail.com>
2017-11-21 14:32:04 -0800

    extracted examples

---
+++ bin/package-init.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+NAME=${NAME:-$1}
+if [ -z $NAME ]; then
+  echo "$0 <name> is required"
+  exit 1
+fi
+VERSION=${VERSION:"3.0.0-b15"}
+
+pkg(){
+if [ -f package.json ]; then
+  echo "package.json exists $NAME";
+  return 1
+fi
+
+cat <<EOF> package.json
+{
+  "name":"$NAME",
+  "version":"$VERSION"
+  "scripts":{
+     "test":"subschema-karma",
+     "karma":"subschema-karma",
+     "build":"subschema-webpack"
+  },
+  "dependencies":{
+    "subschema-util":"^$VERSION"
+  },
+  "devDependencies"{
+    "subschema-dev-support":"^$VERSION"
+  }
+}
+
+
+EOF
+}
+
+readme(){
+  if [ -f README.md ]; then
+    echo "README.md exists in $NAME"
+  fi
+
+cat << EOF > README.md
+${NAME}
+===
+
+## Installation
+\`\`\`sh
+ $ yarn install $NAME
+\`\`\`
+EOF
+}
