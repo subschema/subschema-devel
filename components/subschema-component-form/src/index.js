a1138b1439b418e5c8a6ff1df1cf3c5268df3539
Justin Spears <speajus@gmail.com>
2017-11-21 13:20:53 -0800

    renames

--- components/subschema-component-form/src/index.js
+++ components/subschema-component-form/src/index.js
@@ -1,81 +1,135 @@
-import ButtonTemplate from './templates/ButtonTemplate';
-import ButtonsTemplate from './templates/ButtonsTemplate';
-import CheckboxTemplate from './templates/CheckboxTemplate';
-import CheckboxesGroupTemplate from './templates/CheckboxesGroupTemplate';
-import CheckboxesTemplate from './templates/CheckboxesTemplate';
-import EditorTemplate from './templates/EditorTemplate';
-import FieldSetTemplate from './templates/FieldSetTemplate';
-import FormTemplate from './templates/FormTemplate';
-import RadioItemTemplate from './templates/RadioItemTemplate';
-import ObjectTemplate from './templates/ObjectTemplate';
+import React, { Component } from 'react';
+import PropTypes from 'subschema-prop-types';
+import ObjectType from './Object';
 
-import Checkbox from './types/Checkbox';
-import Checkboxes from './types/Checkboxes';
-import Date from './types/Date';
-import Hidden from './types/Hidden';
-import Number from './types/Number';
-import Password from './types/Password';
-import Radio from './types/Radio';
-import Restricted from './types/Restricted';
-import Select from './types/Select';
-import Text from './types/Text';
-import TextArea from './types/TextArea';
-import Content from 'subschema-core/lib/Content';
-import ObjectType from 'subschema-core/lib/Object';
-import _RestrictedMixin from './types/RestrictedMixin';
-import _Dom from './Dom';
-import _css from './css';
+export default class Form extends Component {
+    static displayName       = "Form";
+    static defaultValueManager;
+    static childContextTypes = {
+        validate  : PropTypes.bool,
+        noValidate: PropTypes.bool, ...PropTypes.contextTypes
+    };
 
-import _resolvers from 'subschema-core/lib/resolvers';
-import _Conditional from 'subschema-core/lib/Conditional';
-import { settings as transitionSettings } from 'subschema-core/lib/resolvers/transition';
-import _ReactCSSReplaceTransition from 'react-transition-group/CSSTransition';
+    static propTypes = {
+        schema      : PropTypes.schema.isRequired,
+        loader      : PropTypes.loader,
+        injector    : PropTypes.injectorFactory,
+        valueManager: PropTypes.valueManager,
+        template    : PropTypes.string,
+        method      : PropTypes.string,
+        action      : PropTypes.string,
+        enctype     : PropTypes.string,
+        //handy submit handler.
+        onSubmit    : PropTypes.event,
+        //Set this to true if you don't want validation to run on submit.
+        noValidate  : PropTypes.bool,
+        //Set this to true, if you want validators to be called against the
+        // current schema.  I.E. after a POST.
+        validate    : PropTypes.bool,
+        ObjectType  : PropTypes.any
+    };
 
-export const Conditional = _Conditional;
+    static defaultProps = {
+        fallbackTemplate: 'FormTemplate',
+        noValidate      : false,
+        validate        : false,
+        ObjectType
+    };
 
-export const resolvers = _resolvers;
+    constructor(props, context, whatever) {
+        super(props, context, whatever);
+        this.loader   = props.loader;
+        this.injector =
+            typeof props.injector === 'function' ? props.injector(this.loader)
+                : props.injector;
+        if (!props.valueManager) {
+            this.valueManager =
+                this.constructor.defaultValueManager(props.value, props.errors);
+        } else {
+            this.valueManager = props.valueManager;
+            if (props.value) {
+                this.valueManager.setValue(props.value);
+            }
+            if (props.errors) {
+                this.valueManager.setErrors(props.errors);
+            }
+        }
+        this.ObjectWrapper = this.injector.inject(this.props.ObjectType);
+        if (props.onSubmit) {
+            this._submitListener = this.valueManager.addSubmitListener(null,
+                props.onSubmit).remove;
+        }
+    }
 
-export const ReactCSSReplaceTransition = Conditional.Transition =
-    transitionSettings.Transition = _ReactCSSReplaceTransition;
+    getChildContext() {
+        return {
+            valueManager: this.valueManager,
+            loader      : this.loader,
+            injector    : this.injector,
+            validate    : this.props.validate,
+            noValidate  : this.props.noValidate
+        };
+    }
+
+    componentWillReceiveProps(newProps) {
+
+        if (newProps.loader !== this.props.loader) {
+            this.loader = newProps.loader;
+        }
+        if (newProps.valueManager !== this.props.valueManager) {
+            if (this._submitListener) {
+                this._submitListener();
+            }
+            this.valueManager = newProps.valueManager;
+            this.forceUpdate();
+        }
+
+        if (this.props.value !== newProps.value) {
+            this.valueManager.setValue(newProps.value);
+        }
+        if (this.props.errors !== newProps.errors) {
+            this.valueManager.setErrors(newProps.errors);
+        }
+        if (this.props.injector !== newProps.injector || this.props.loader
+                                                         !== newProps.loader) {
+            if (typeof newProps.injector === 'function') {
+                this.injector = newProps.injector(newProps.loader);
+            } else {
+                this.injector = newProps.injector;
+            }
+            this.ObjectWrapper = this.injector.inject(this.props.ObjectType);
+        }
+
+        if (newProps.onSubmit) {
+            if (this._submitListener) {
+                this._submitListener();
+            }
+            this._submitListener = this.valueManager.addSubmitListener(null,
+                newProps.onSubmit).remove;
+        }
+    }
+
+    componentWillUnmount() {
+        this._submitListener && this._submitListener();
+    }
+
+    getValue() {
+        return this.valueManager.getValue();
+    }
+
+    setErrors = (errors) => {
+        this.valueManager.setErrors(errors);
+    };
 
 
-export const RestrictedMixin = _RestrictedMixin;
+    render() {
 
-export const Dom = _Dom;
+        const {
+                  valueManager, injector, loader, validate,
+                  template, onSubmit, ...props
+              }             = this.props;
+        const ObjectWrapper = this.ObjectWrapper;
+        return <ObjectWrapper {...props} objectTemplate={template}/>
+    }
 
-export const css = _css;
-
-
-export const templates = {
-    ButtonTemplate,
-    ButtonsTemplate,
-    CheckboxTemplate,
-    CheckboxesGroupTemplate,
-    CheckboxesTemplate,
-    EditorTemplate,
-    FieldSetTemplate,
-    FormTemplate,
-    ObjectTemplate,
-    RadioItemTemplate,
-};
-export const types     = {
-    Content,
-    Checkbox,
-    Checkboxes,
-    Date,
-    Hidden,
-    Number,
-    Object: ObjectType,
-    ObjectType,
-    Password,
-    Radio,
-    Restricted,
-    Select,
-    Text,
-    TextArea,
-};
-
-export default ({
-    types,
-    templates
-});
+}
