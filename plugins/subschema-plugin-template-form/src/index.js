6dcfe543b7fdc18b94a730a951324bd4562bc3ca
Justin Spears <jspears@netflix.com>
2019-03-20 16:14:22 -0700

    make autoComplete compatible

--- plugins/subschema-plugin-template-form/src/index.js
+++ plugins/subschema-plugin-template-form/src/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "subschema-prop-types";
+import React, {Component} from "react";
+import PropTypes          from "subschema-prop-types";
 
 export class FormTemplate extends Component {
 
@@ -15,6 +15,7 @@ export class FormTemplate extends Component {
         autocapitalize: PropTypes.oneOf(
             ['on', 'off', 'words', 'sentences', 'charecters', 'none']),
         autoComplete  : PropTypes.oneOf(['on', 'off']),
+        autocomplete  : PropTypes.deprecated('Please use autoComplete instead'),
         encType       : PropTypes.oneOf(
             ['application/x-www-form-urlencoded', 'multipart/form-data', 'text/plain']),
         method        : PropTypes.oneOf(['get', 'post']),
@@ -34,7 +35,10 @@ export class FormTemplate extends Component {
     };
 
     render() {
-        const { children, style, fieldAttrs, formClass, className, ...props } = this.props;
+        const {children, style, fieldAttrs, autocomplete, formClass, className, ...props} = this.props;
+        if (autocomplete && !props.autoComplete) {
+            props.autoComplete = autocomplete;
+        }
         return (
             <form {...props} className={className || formClass} {...fieldAttrs}>
                 {children}
