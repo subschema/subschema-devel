f2047f70a6a6fe0f0a214640f46740e718f3c69b
Justin Spears <jspears@netflix.com>
2019-05-06 16:51:33 -0700

    fixes  for build

--- core/subschema-dev-support/bin/subschema-babel.js
+++ core/subschema-dev-support/bin/subschema-babel.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
-process.env.MRBUILDER_INTERNAL_PRESETS='subschema-dev-support';
+process.env.MRBUILDER_INTERNAL_PRESETS = 'subschema-dev-support';
 process.env.MRBUILDER_PROFILE = 'babel';
+if (!(process.argv.slice(2).includes('-f') || process.argv.slice(2).includes('--file'))) {
+    process.argv.splice(2, 0, '-f', '-');
+}
 require('mrbuilder/bin/mrbuilder');
